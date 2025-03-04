45ac86bda5930fbfc45a6f0f0835365a8612d68a
Thomas Chagneux <Thomas.chagneux@gmail.com>
2025-03-04 17:20:59 +0100

    P6-21 create homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller;
 
+use App\Repository\FigureRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
+    public function __construct(
+        private readonly FigureRepository $figureRepository,
+    ) {
+    }
+
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        $figures = $this->figureRepository->findAll();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'figures' => $figures,
         ]);
     }
 }
